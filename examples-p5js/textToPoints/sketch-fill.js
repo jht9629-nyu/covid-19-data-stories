9a3c0d76c8f7fefb2a3f23cc50e32a60722c1190
jht <jht@jht-mbpro.home>
2021-02-13 22:55:44 -0500

    relocate examples

--- examples-p5js/textToPoints/sketch-fill.js
+++ examples-p5js/textToPoints/sketch-fill.js
@@ -1,49 +1,41 @@
 let font;
 function preload() {
-  // font = loadFont('../assets/inconsolata.otf');
+  // loadJSON_brooklyn_shape/assets/ChunkFive-Regular.otf
+  // font = loadFont('assets/inconsolata.otf');
   font = loadFont('../assets/ChunkFive-Regular.otf');
 }
 
 let points;
 let bounds;
-
 function setup() {
   createCanvas(400, 500);
   stroke(0);
-  strokeWeight(1);
-
+  strokeWeight(10);
   // fill(255, 104, 204);
   fill('gray');
   let tsize = 100;
-  let ttxt = 'B E';
+  let ttxt = '9';
   points = font.textToPoints(ttxt, 0, 0, tsize, {
-    sampleFactor: 0.8,
+    sampleFactor: 5,
     simplifyThreshold: 0,
   });
   bounds = font.textBounds(' ' + ttxt + ' ', 0, 0, tsize);
-  // bounds = font.textBounds( ttxt , 0, 0, tsize);
-  console.log('points.length', points.length);
-  console.log('bounds', bounds);
+  console.log('points', points);
 }
-
-let x_margin = 10;
-let y_margin = 10;
 
 function draw() {
   background(255);
-  // beginShape();
+  beginShape();
   translate((-bounds.x * width) / bounds.w, (-bounds.y * height) / bounds.h);
   for (let i = 0; i < points.length; i++) {
     let p = points[i];
-    let x = (p.x * width) / bounds.w;
-    x += (sin((20 * p.y) / bounds.h + millis() / 1000) * width) / 200;
-    x += x_margin;
-    let y = (p.y * (height - y_margin)) / bounds.h;
-    // y += y_margin;
-    circle(x, y, 5);
+    vertex(
+      (p.x * width) / bounds.w +
+        (sin((20 * p.y) / bounds.h + millis() / 1000) * width) / 30,
+      (p.y * height) / bounds.h
+    );
   }
-  // endShape(CLOSE);
+  endShape(CLOSE);
 }
 
 // https://p5js.org/reference/#/p5.Font/textToPoints
-// https://p5js.org/reference/#/p5.Font/textBounds
